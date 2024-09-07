30bf55c074821cea1b5a0c20982d495e6bb34ac1
Ralf Krümmel <ralf.kruemmel+python@outlook.de>
2024-09-07 05:57:31 +0200

    Bitmanipulation Funktionen
    Dieses C++-Projekt demonstriert verschiedene Techniken der **Bitmanipulation** und **Bitmasken-Anwendung**. Es wurde modular strukturiert, um die Lesbarkeit und Wartbarkeit des Codes zu verbessern.

---
+++ KT040924Kruemmel/Funktionen/masken.h
@@ -0,0 +1,9 @@
+#ifndef MASKEN_H
+#define MASKEN_H
+
+// Deklaration der Funktionen für Bitmaskenoperationen
+int applyMaskAdd(int number, int mask);
+int applyMaskClear(int number, int mask);
+int applyMaskCheck(int number, int mask);
+
+#endif
